c914290269bfd0b420eb97de73ad220b6838a515
Jess <100146999+JessieMic@users.noreply.github.com>
2023-03-21 16:56:46 +0400

    Created a better base to start our server

---
+++ FinalProjectLobby/GameHub.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+public class GameHub : Hub
+{
+    public static string[] buttonsThatAreOccupied = new string[4];
+    //public static int amountOfPlayersThatAreReady = 0;
+
+    public async Task TryPickAScreenSpot(string nameOfClient, String numberOfButton, string screenWidth, string screenHeight)
+    {
+        int chosenButtonNumber;
+        double width = double.Parse(screenWidth);
+        double height = double.Parse(screenHeight);
+
+        if (int.TryParse(numberOfButton, out chosenButtonNumber))
+        {
+            chosenButtonNumber--;
+
+            if (buttonsThatAreOccupied[chosenButtonNumber] == String.Empty
+                || buttonsThatAreOccupied[chosenButtonNumber] == null)
+            {
+                //Client can pick the spot so we will update all of the clients
+                buttonsThatAreOccupied[chosenButtonNumber] = nameOfClient;
+
+                await Clients.All.SendAsync("PlacementUpdateRecevied", nameOfClient,
+                chosenButtonNumber, width, height);
+                //amountOfPlayersThatAreReady++;
+            }
+
+            //if (amountOfPlayersThatAreReady >= 4)
+            //{
+            //    await Clients.All.SendAsync("GameIsAboutToStart");
+            //}
+        }
+    }
+
+    public async Task TryToDeselectScreenSpot(string nameOfClient, String numberOfpreviousChosenButton,
+        String buttonThatClientWantsToDeselect)
+    {
+        int chosenButtonNumber;
+
+        if (int.TryParse(numberOfpreviousChosenButton, out chosenButtonNumber))
+        {
+            chosenButtonNumber--;
+
+            if (buttonThatClientWantsToDeselect == nameOfClient)
+            {
+                buttonsThatAreOccupied[chosenButtonNumber] = string.Empty;
+                await Clients.All.SendAsync("DeSelectPlacementUpdatReceived", nameOfClient,
+                chosenButtonNumber);
+                //amountOfPlayersThatAreReady--;
+            }
+        }
+    }
+
+    public async Task RequestScreenUpdate(string clientId)
+    {
+        await Clients.Client(clientId).SendAsync("RecieveScreenUpdate", buttonsThatAreOccupied);
+    }
+
+    public async Task GameIsAboutToStart()
+    {
+        buttonsThatAreOccupied[0] = string.Empty;
+        buttonsThatAreOccupied[1] = string.Empty;
+        buttonsThatAreOccupied[2] = string.Empty;
+        buttonsThatAreOccupied[3] = string.Empty;
+        await Clients.All.SendAsync("StartGame");
+    }
+
+
+}
--- FinalProjectLobby/Program.cs
+++ FinalProjectLobby/Program.cs
@@ -4,22 +4,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.MapHub<GameHub>("/gamehub");
 
-app.MapControllers();
+
+
 
 app.Run();
